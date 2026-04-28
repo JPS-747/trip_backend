17aec60a25f9b0278d91ebf1aaa65bd3eb884efc
JPS-747 <jps@aixaro.com>
2026-04-28 17:28:41 +0200

    Cors setup changed

--- api.py
+++ api.py
@@ -130,7 +130,9 @@ api = FastAPI(
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://tripfrontend-production.up.railway.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
